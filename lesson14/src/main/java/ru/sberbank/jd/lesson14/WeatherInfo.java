517b57d71735f03112fbf9bd380cace2429d6a0b
Artem <olimpicboy17@gmail.com>
2023-12-09 21:09:26 +0300

    HomeWork lesson14 fix

--- lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherInfo.java
+++ lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherInfo.java
@@ -3,47 +3,165 @@ package ru.sberbank.jd.lesson14;
 import java.time.LocalDateTime;
 
 /**
- * Weather info.
+ * Информация о погоде.
  */
 public class WeatherInfo {
 
     private String city;
 
     /**
-     * Short weather description.
-     * Like 'sunny', 'clouds', 'raining', etc
+     * Краткое описание погоды.
+     * * Например, "солнечно", "облачно", "дождь" и т.д
      */
     private String shortDescription;
 
     /**
-     * Weather description.
-     * Like 'broken clouds', 'heavy raining', etc
+     * Описание погоды.
+     * Например, "разорванные облака", "сильный дождь" и т.д
      */
     private String description;
 
     /**
-     * Temperature.
+     * Температура.
      */
     private double temperature;
 
     /**
-     * Temperature that fells like.
+     * Температура, которая ощущается как.
      */
     private double feelsLikeTemperature;
 
     /**
-     * Wind speed.
+     * Скорость ветра.
      */
     private double windSpeed;
 
     /**
-     * Pressure.
+     * Давление.
      */
     private double pressure;
 
     /**
-     * Expiry time of weather info.
-     * If current time is above expiry time then current weather info is not actual!
+     * Время истечения срока действия информации о погоде.
+     * Если текущее время превышает время истечения срока действия, то текущая информация о погоде не актуальна!
      */
     private LocalDateTime expiryTime;
+
+    /**
+     * Конструктор.
+     */
+    private WeatherInfo(WeatherInfoBuilder builder) {
+        this.city = builder.city;
+        this.shortDescription = builder.shortDescription;
+        this.description = builder.description;
+        this.temperature = builder.temperature;
+        this.feelsLikeTemperature = builder.feelsLikeTemperature;
+        this.windSpeed = builder.windSpeed;
+        this.pressure = builder.pressure;
+        this.expiryTime = builder.expiryTime;
+    }
+
+    /**
+     * Builder класс.
+     */
+    public static class WeatherInfoBuilder {
+        private String city;
+        private String shortDescription;
+        private String description;
+        private double temperature;
+        private double feelsLikeTemperature;
+        private double windSpeed;
+        private double pressure;
+        private LocalDateTime expiryTime;
+
+
+        public WeatherInfoBuilder(String city) {
+            this.city = city;
+        }
+
+        public WeatherInfoBuilder shortDescription(String shortDescription) {
+            this.shortDescription = shortDescription;
+            return this;
+        }
+
+        public WeatherInfoBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public WeatherInfoBuilder temperature(double temperature) {
+            this.temperature = temperature;
+            return this;
+        }
+
+        public WeatherInfoBuilder feelsLikeTemperature(double feelsLikeTemperature) {
+            this.feelsLikeTemperature = feelsLikeTemperature;
+            return this;
+        }
+
+        public WeatherInfoBuilder windSpeed(double windSpeed) {
+            this.windSpeed = windSpeed;
+            return this;
+        }
+
+        public WeatherInfoBuilder pressure(double pressure) {
+            this.pressure = pressure;
+            return this;
+        }
+
+        public WeatherInfoBuilder expiryTime(LocalDateTime expiryTime) {
+            this.expiryTime = expiryTime;
+            return this;
+        }
+
+        public WeatherInfo build() {
+            return new WeatherInfo(this);
+        }
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getFeelsLikeTemperature() {
+        return feelsLikeTemperature;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public LocalDateTime getExpiryTime() {
+        return expiryTime;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherInfo{"
+                + "city='" + city + '\''
+                + ", shortDescription='" + shortDescription + '\''
+                + ", description='" + description + '\''
+                + ", temperature=" + temperature
+                + ", feelsLikeTemperature=" + feelsLikeTemperature
+                + ", windSpeed=" + windSpeed
+                + ", pressure=" + pressure
+                + ", expiryTime=" + expiryTime
+                + '}';
+    }
 }
--- lesson14/src/test/java/WeatherProviderCacheTest.java
+++ lesson14/src/test/java/WeatherProviderCacheTest.java
@@ -1,11 +1,64 @@
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import ru.sberbank.jd.lesson14.WeatherCache;
+import ru.sberbank.jd.lesson14.WeatherInfo;
+import ru.sberbank.jd.lesson14.WeatherProvider;
+import java.time.LocalDateTime;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-public class WeatherProviderCache {
 
-    @Test
-    public void parseWeatherInfoTest(){
+public class WeatherProviderCacheTest {
 
+    private static WeatherProvider weatherProvider = mock(WeatherProvider.class);
+    private static WeatherInfo weatherInfoAfter;
+    private static WeatherInfo weatherInfoBefore;
 
+    private static WeatherCache weatherCache;
+
+    @BeforeClass
+    public static void init() {
+        weatherInfoAfter = new WeatherInfo.WeatherInfoBuilder("Moscow")
+                .temperature(260.11)
+                .shortDescription("Clouds")
+                .description("overcast clouds")
+                .feelsLikeTemperature(256.39)
+                .expiryTime(LocalDateTime.now().minusMinutes(6))
+                .windSpeed(5)
+                .pressure(1042.0)
+                .build();
+
+        weatherInfoBefore = new WeatherInfo.WeatherInfoBuilder("Moscow")
+                .temperature(100.10)
+                .shortDescription("No clouds")
+                .description("overcast clouds")
+                .feelsLikeTemperature(256.39)
+                .expiryTime(LocalDateTime.now())
+                .windSpeed(1)
+                .pressure(1042.0)
+                .build();
+
+        when(weatherProvider.get("Moscow")).thenReturn(weatherInfoAfter);
+        weatherCache = new WeatherCache(weatherProvider);
     }
 
+    @Test
+    public void cacheEmptyTest() {
+        WeatherInfo test = weatherCache.getWeatherInfo("Moscow");
+        Assert.assertEquals("Moscow", test.getCity());
+        Assert.assertEquals((int) 260.11, (int) test.getTemperature());
+        Assert.assertEquals("Clouds", test.getShortDescription());
+    }
+
+    @Test
+    public void updateInfoTest() {
+        when(weatherProvider.get("Moscow")).thenReturn(weatherInfoBefore);
+
+        WeatherInfo test = weatherCache.getWeatherInfo("Moscow");
+        Assert.assertEquals("Moscow", test.getCity());
+        Assert.assertEquals((int) 100.10, (int) test.getTemperature());
+        Assert.assertEquals("No clouds", test.getShortDescription());
+    }
 }
